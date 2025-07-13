7ec430f89fc8c8d4b9502f06088edb9a87914fab
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2025-07-13 16:44:26 +0700

    feat : add lottiefiles scroll icons

---
+++ src/components/ui/ScrollIcon.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import lightAnimation from "@/public/icons/chevron_light.json";
+import darkAnimation from "@/public/icons/chevron_dark.json";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes"; // Perbaiki import
+import Lottie from "lottie-react";
+
+export default function ScrollIcon() {
+  const { resolvedTheme } = useTheme(); // Gunakan resolvedTheme untuk lebih reliable
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Hindari mismatch antara server dan client
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  // Perbaiki kondisi: light theme = lightAnimation, dark theme = darkAnimation
+  const animationData =
+    resolvedTheme === "dark" ? lightAnimation : darkAnimation;
+
+  return (
+    <div className="w-8 h-8 rotate-90 border-1 rounded">
+      <Lottie 
+        animationData={animationData} 
+        loop 
+        autoplay
+      />
+    </div>
+  );
+}
