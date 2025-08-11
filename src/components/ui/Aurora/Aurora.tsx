9d1bc6188bd6a420f760c6107555ce281d1f5415
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2025-08-11 20:15:59 +0700

    fix: fix aurora error

--- src/components/ui/Aurora/Aurora.tsx
+++ src/components/ui/Aurora/Aurora.tsx
@@ -141,7 +141,8 @@ export default function Aurora(props: AuroraProps) {
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
     gl.canvas.style.backgroundColor = "transparent";
-
+    
+    // eslint-disable-next-line prefer-const
     let program: Program | undefined;
 
     function resize() {
